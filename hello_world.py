875ef68dd92f3dcf081390809820fbaa275560af
Dao Trung Hieu <43900080+hieutrungdao@users.noreply.github.com>
2022-09-09 01:06:54 +0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,42 @@
-print("Okeee")
+import sys
+import uuid
+import os
+import datetime
+import base64
+
+import uvicorn
+from fastapi import FastAPI, HTTPException, status, Request, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+from pprint import pprint
+
+
+#create app
+app = FastAPI(title='VisualAI')
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/") 
+async def root(): 
+    return {"message": "Hello World"}
+
+
+@app.post("/input")
+async def get_body(request: Request):
+    data = await request.json()
+    pprint(data)
+    return "OK"
+    # return await request.json()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9452)
